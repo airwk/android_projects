ef6f08877d31dee39ecf798af6ac6ce8bc6565cd
airwk <airtaro.suzumiya@gmail.com>
2013-07-05 18:13:54 +0700

    Revert "Revert "Revert "a"""
    This reverts commit 424d309a73af52eaf40eb9931e585be710276ade.

---
+++ Game2/src/game/core/Resources.java
@@ -0,0 +1,20 @@
+package game.core;
+
+public final class Resources
+{
+	private Resources()
+	{}
+	
+	public static final String SPLASHSCREEN_ROOTPATH = "gfx/splashscreen/";
+	public static final String SPLASHSCREEN_BACKGROUND = SPLASHSCREEN_ROOTPATH + "bg.jpg";
+	public static final String SPLASHSCREEN_LOGO = SPLASHSCREEN_ROOTPATH + "logo.png";
+	public static final String SPLASHSCREEN_NOWLOADING_TEXT = SPLASHSCREEN_ROOTPATH + "nowloading.png";
+	public static final String SPLASHSCREEN_LOADING_DOT = SPLASHSCREEN_ROOTPATH + "dot.png";
+	
+	public static final String MAINMENU_ROOTPATH = "gfx/mainmenu/";
+	public static final String MAINMENU_BACKGROUND = MAINMENU_ROOTPATH + "bg.png";
+	public static final String MAINMENU_LOGO = MAINMENU_ROOTPATH + "logo.png";
+	public static final String MAINMENU_START_BUTTON = MAINMENU_ROOTPATH + "start_btn.png";
+	public static final String MAINMENU_CREDIT_BUTTON = MAINMENU_ROOTPATH + "credit_btn.png";
+	public static final String MAINMENU_EXIT_BUTTON = MAINMENU_ROOTPATH + "exit_btn.png";
+}
